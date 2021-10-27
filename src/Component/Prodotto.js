b0a4512cbd34759d4157dd61ba9a7044db50d9ad
Loris Pozzuoli <lorispozzuoli@gmail.com>
2021-10-27 11:54:20 +0200

    funziona il router

--- src/Component/Prodotto.js
+++ src/Component/Prodotto.js
@@ -19,31 +19,32 @@ export default function Prodotto(props) {
         <Link
           to={`/prodotto/${prodotto.UPC}`}
           style={{ textDecoration: "none", color: "black" }}
-        ></Link>
-        {/* <CardActionArea
+        >
+          {/* <CardActionArea
           onClick={() => history.push(`/prodotto/${prodotto.UPC}`)}
         > */}
-        <CardMedia
-          component="img"
-          image="https://via.placeholder.com/350"
-          alt={prodotto.name}
-        />
-        <CardContent>
-          <Typography variant="h5" component="div">
-            {prodotto.name}
-          </Typography>
-          <Typography sx={{ mb: 1.5 }} color="text.secondary">
-            $ {prodotto.price.current.value}
-          </Typography>
-          <Typography sx={{ mb: 1.5 }} color="text.secondary">
-            {prodotto.availability.stock > 0 ? (
-              <Chip label="In stock" />
-            ) : (
-              <Chip label="Out of stock" />
-            )}
-          </Typography>
-        </CardContent>
-        {/* </CardActionArea> */}
+          <CardMedia
+            component="img"
+            image="https://via.placeholder.com/350"
+            alt={prodotto.name}
+          />
+          <CardContent>
+            <Typography variant="h5" component="div">
+              {prodotto.name}
+            </Typography>
+            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+              $ {prodotto.price.current.value}
+            </Typography>
+            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+              {prodotto.availability.stock > 0 ? (
+                <Chip label="In stock" />
+              ) : (
+                <Chip label="Out of stock" />
+              )}
+            </Typography>
+          </CardContent>
+          {/* </CardActionArea> */}
+        </Link>
       </Card>
     </Grid>
   );
