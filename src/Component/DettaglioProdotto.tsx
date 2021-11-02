b9c8ef9a54123d49156c3707c2b1bce2ce8bee7d
Loris Pozzuoli <lorispozzuoli@gmail.com>
2021-11-02 11:53:51 +0100

    funziona modificato nome const dettaglio prodotto

--- src/Component/DettaglioProdotto.tsx
+++ src/Component/DettaglioProdotto.tsx
@@ -3,11 +3,10 @@ import Prodotto from "./Prodotto";
 import { useParams } from "react-router-dom";
 import { prodottiTot } from "../Data";
 
-
 const DettaglioProdotto: React.FC = () => {
   const { UPC } = useParams<{ UPC: string }>();
-  const prodotto = prodottiTot.find((prodotto) => prodotto.UPC === UPC);
-  return prodotto ? <Prodotto prodotto={prodotto} /> : null;
+  const product = prodottiTot.find((prodotto) => prodotto.UPC === UPC);
+  return product ? <Prodotto prodotto={product} /> : null;
 };
 
 export default DettaglioProdotto;
