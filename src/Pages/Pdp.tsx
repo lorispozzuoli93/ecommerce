5b7eb39d9baefdb0936c81fca0d467f83a958fe1
Loris Pozzuoli <lorispozzuoli@gmail.com>
2021-11-02 12:31:04 +0100

    mancava il nome prodotto

--- src/Pages/Pdp.tsx
+++ src/Pages/Pdp.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Prodotto from "../Component/Product";
+import Product from "../Component/Product";
 import { useParams } from "react-router-dom";
 import { allProducts } from "../Model/Data";
 
 const Pdp: React.FC = () => {
   const { UPC } = useParams<{ UPC: string }>();
   const productFind = allProducts.find((prodotto) => prodotto.UPC === UPC);
-  return productFind ? <Prodotto product={productFind} /> : null;
+  return productFind ? <Product product={productFind} /> : null;
 };
 
 export default Pdp;
--- src/Pages/Plp.tsx
+++ src/Pages/Plp.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { useState } from "react";
 import Grid from "@mui/material/Grid";
-import Prodotto from "../Component/Product";
+import Product from "../Component/Product";
 import NavBar from "../Component/NavBar";
 import { allProducts } from "../Model/Data";
 import Footer from "../Component/Footer";
@@ -14,36 +14,34 @@ const Plp: React.FC = () => {
   const [toggle, setToggle] = useState<string>("none");
 
   return (
-      <Grid container direction="column" minHeight="100vh">
-        <NavBar
-          setToggle={setToggle}
-          toggle={toggle}
-          searchQuery={searchQuery}
-          setSearchQuery={setSearchQuery}
-        />
-        <Grid container pl={2} pt={2} spacing={{ xs: 2, md: 2 }} flex={1}>
-          {products
-            ?.filter((prod) => {
-              switch (toggle) {
-                case "in":
-                  return prod.availability.stock > 0;
-                case "out":
-                  return prod.availability.stock <= 0;
-                default:
-                  return true;
-              }
-            })
-            .filter((prod) => {
-              return prod.name
-                .toLowerCase()
-                .includes(searchQuery.toLowerCase());
-            })
-            .map((product, index) => (
-              <Prodotto product={product} key={index} />
-            ))}
-        </Grid>
-        <Footer />
+    <Grid container direction="column" minHeight="100vh">
+      <NavBar
+        setToggle={setToggle}
+        toggle={toggle}
+        searchQuery={searchQuery}
+        setSearchQuery={setSearchQuery}
+      />
+      <Grid container pl={2} pt={2} spacing={{ xs: 2, md: 2 }} flex={1}>
+        {products
+          ?.filter((prod) => {
+            switch (toggle) {
+              case "in":
+                return prod.availability.stock > 0;
+              case "out":
+                return prod.availability.stock <= 0;
+              default:
+                return true;
+            }
+          })
+          .filter((prod) => {
+            return prod.name.toLowerCase().includes(searchQuery.toLowerCase());
+          })
+          .map((product, index) => (
+            <Product product={product} key={index} />
+          ))}
       </Grid>
+      <Footer />
+    </Grid>
   );
 };
 
