40d0f0c52901d57bd7882180d772170c10ea04fe
Loris Pozzuoli <lorispozzuoli@gmail.com>
2021-11-17 10:43:27 +0100

    prova cambio click foto finire responsive card

--- src/Component/CardPdp.tsx
+++ src/Component/CardPdp.tsx
@@ -2,42 +2,70 @@ import * as React from "react";
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { Products } from "../Data/Data";
+import Footer from "./Footer";
+import NavBar from "./NavBar";
+
+const Grid = styled.div`
+  min-height: 100vh;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+`;
 
 const Container = styled.div`
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-columns: 1fr 1fr;
   grid-template-rows: auto;
   gap: 10px;
   column-gap: 20px;
-  margin-left: 50px;
-  margin-right: 50px;
+  margin-left: 2.5%;
+  margin-right: 2.5%;
   margin-top: 20px;
 `;
 
 const ContainerTitle = styled.div`
   grid-column: 1/4;
+  @media (max-width: 540px) {
+    text-align: center;
+  }
 `;
 
-const Typography = styled.h5`
+const TypographyTitle = styled.h5`
   margin: 0px;
   font-size: 40px;
   font-weight: bold;
 `;
 
+const Typography = styled.h5`
+  margin: 0px;
+  font-size: 35px;
+  font-weight: bold;
+`;
+
 const ContainerMedia = styled.div`
   grid-column: 1/2;
+  @media (max-width: 540px) {
+    grid-column: 1/4;
+    text-align: center;
+  }
 `;
 
 const CardMedia = styled.img`
-
+  @media (max-width: 540px) {
+    height: 350px;
+    width: 350px;
+  }
 `;
 
 const ContainerText = styled.div`
   margin-right: 100px;
+  @media (max-width: 540px) {
+    margin-right: 0px;
+  }
 `;
 
 const TypographyPrice = styled.p`
-  font-size: 35px;
+  font-size: 30px;
   font-weight: bold;
   margin-top: 0;
 `;
@@ -62,10 +90,19 @@ const ButtonAddCart = styled.button`
   font-size: 22px;
   font-weight: bold;
   margin-top: 25px;
+  @media (max-width: 540px) {
+    position: relative;
+    left: 300px;
+    bottom: 75px;
+  }
 `;
 
 const ContainerColor = styled.div`
   grid-column: 1/4;
+  @media (max-width: 540px) {
+    position: relative;
+    bottom: 65px;
+  }
 `;
 
 const TypographyColors = styled.h5`
@@ -76,9 +113,14 @@ const TypographyColors = styled.h5`
 `;
 
 const GridImage = styled.div`
-  display: grid;
-  grid-template-columns: 11.11% 11.11% 11.11% 11.11% 11.11% 11.11% 11.11% 11.11% 11.11%;
-  cursor: pointer;
+  grid-column: 1/4;
+  & img {
+    cursor: pointer;
+  }
+  margin-bottom: 100px;
+  @media (max-width: 540px) {
+    margin-bottom: 25px;
+  }
 `;
 
 type Props = {
@@ -93,52 +135,60 @@ const CardPdp: React.FC<Props> = ({ product }) => {
   // const variant = useSelector(variantsSelector);
   // const dispatch = useDispatch();
   return (
-    <Container>
-      {variant ? (
-        <>
-          <ContainerTitle>
-            <Typography>{variant.name}</Typography>
-          </ContainerTitle>
-          <ContainerMedia>
-            <CardMedia src="https://via.placeholder.com/1000x600" />
-          </ContainerMedia>
-          <ContainerText>
-            <Typography>{variant.name}</Typography>
-            <TypographyPrice>{variant.price.current.value} USD</TypographyPrice>
-            <TypographyName>
-              <span>Name:</span> {variant.name}
-            </TypographyName>
-            <TypographyName>
-              <span>Lens color:</span> Green
-            </TypographyName>
-            <TypographyName>
-              <span>Size:</span> Standard
-            </TypographyName>
-            <TypographyName>
-              <span>UPC:</span> {variant.UPC}
-            </TypographyName>
-            <ButtonAddCart>Add to cart</ButtonAddCart>
-          </ContainerText>
-          <ContainerColor>
-            <TypographyColors>Available colors:</TypographyColors>
-            <GridImage>
-              <img
-                alt={`Product${product.UPC}`}
-                src={`https://picsum.photos/200/200?random=${product.UPC}`}
-                onClick={() => setVariant(product)}
-              />
-              {product.variants.map((prod) => (
-                <img
-                  alt={`Product${prod.UPC}`}
-                  src={`https://picsum.photos/200/200?random=${prod.UPC}`}
-                  onClick={() => setVariant(prod)}
-                />
-              ))}
-            </GridImage>
-          </ContainerColor>
-        </>
-      ) : null}
-    </Container>
+    <Grid>
+      <>
+        <NavBar />
+        <Container>
+          {variant ? (
+            <>
+              <ContainerTitle>
+                <TypographyTitle>{variant.name}</TypographyTitle>
+              </ContainerTitle>
+              <ContainerMedia>
+                <CardMedia src="https://via.placeholder.com/1000x600" />
+              </ContainerMedia>
+              <ContainerText>
+                <Typography>{variant.name}</Typography>
+                <TypographyPrice>
+                  {variant.price.current.value} USD
+                </TypographyPrice>
+                <TypographyName>
+                  <span>Name:</span> {variant.name}
+                </TypographyName>
+                <TypographyName>
+                  <span>Lens color:</span> Green
+                </TypographyName>
+                <TypographyName>
+                  <span>Size:</span> Standard
+                </TypographyName>
+                <TypographyName>
+                  <span>UPC:</span> {variant.UPC}
+                </TypographyName>
+                <ButtonAddCart>Add to cart</ButtonAddCart>
+              </ContainerText>
+              <ContainerColor>
+                <TypographyColors>Available colors:</TypographyColors>
+                <GridImage>
+                  <img
+                    alt={`Product${product.UPC}`}
+                    src={`https://picsum.photos/200/200?random=${product.UPC}`}
+                    onClick={() => setVariant(product)}
+                  />
+                  {product.variants.map((prod) => (
+                    <img
+                      alt={`Product${prod.UPC}`}
+                      src={`https://picsum.photos/200/200?random=${prod.UPC}`}
+                      onClick={() => setVariant(prod)}
+                    />
+                  ))}
+                </GridImage>
+              </ContainerColor>
+            </>
+          ) : null}
+        </Container>
+      </>
+      <Footer />
+    </Grid>
   );
 };
 
